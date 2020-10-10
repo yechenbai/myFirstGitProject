eeb0d222b5c0fcf4533c51e6cc23b4d8e38886b1
huzhongbao <2397592318@qq.com>
2020-10-10 12:04:17 +0800

    第一次发布

---
+++ WindowsFormsApp3/fushu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class fushu
+    {
+        public double sb;
+        public double xb;
+        public fushu(double sb, double xb)
+        {
+            this.sb = sb;
+            this.xb = xb;
+        }
+        public static fushu operator +(fushu a, fushu b)
+        {
+            return new fushu(a.sb + b.sb, a.xb + b.xb);
+        }
+        public static fushu operator -(fushu a, fushu b)
+        {
+            return new fushu(a.sb - b.sb, a.xb - b.xb);
+        }
+        public static fushu operator *(fushu a, fushu b)
+        {
+            return new fushu(a.sb * b.sb + a.sb * b.xb, a.xb * b.sb + a.xb * b.xb);
+        }
+        public static fushu operator /(fushu a, fushu b)
+        {
+            return new fushu((a.sb * b.sb + a.xb * b.xb) / (b.sb * b.xb), (a.xb * b.sb - a.sb * b.xb) / (b.sb * b.xb));
+        }
+        public override string ToString()
+        {
+            return (String.Format("{0}+{1}i", sb, xb));
+        }
+    }
+}
---
+++ WindowsFormsApp3/juzhen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class juzhen
+    {
+       /// public int a;
+        ///public int b;
+        ///public int [,] arr1=new int[3,4];
+        ///public int [,] arr2=new int[3,4];
+        public juzhen(int arr1, int arr2)
+        {
+            ///this.arr1 = arr1;
+            ///this.arr2 = arr2;
+        }
+    }
+}
